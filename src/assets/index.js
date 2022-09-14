6164e58c205928e097cb45bb545989d13ebfb384
darleneDiPierro <rosequinzley@gmail.com>
2022-09-14 19:31:30 +0700

    first commit

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,7 +2,7 @@ import logo from './iceberg.png'
 import labuena from './labuena.png'
 import aone from './aone.png'
 import mainmenu from './main-menu.png'
-import cv from './AgnesMariaCV.pdf'
+import cv from './AgnesMariaAnggelina.pdf'
 
 export {
     logo,
