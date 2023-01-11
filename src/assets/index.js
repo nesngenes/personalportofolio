2cfc7b686e238dced2fd7778074dcd21b5a503f0
darleneDiPierro <rosequinzley@gmail.com>
2023-01-11 23:39:25 +0700

    changes

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,11 +3,15 @@ import labuena from './labuena.png'
 import aone from './aone.png'
 import mainmenu from './main-menu.png'
 import cv from './AgnesMariaAnggelina.pdf'
+import labuenaver2 from './labuena-ver-2.png'
+import albatech from './albatech.png'
 
 export {
     logo,
     labuena,
     aone,
     mainmenu,
-    cv
+    cv,
+    labuenaver2,
+    albatech
 }
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './styles.css'
-import { labuena, aone } from '../../assets'
+import { labuenaver2, aone, albatech } from '../../assets'
 
 
 
@@ -17,17 +17,36 @@ const Projects = () => {
               <p>Cafe, Map API</p>
               <h1>LaBuena Cafe</h1>
               <div className='image-wrapper'>
-              <img src={labuena} />
+              <img src={labuenaver2} />
               </div>
             </div>
             <div className='project-cotent'>
               <p className='project-description'>
               LaBuena merupakan website cafe yang saya buat menggunakan MongoDB, Express.js, React, Node.js (MERN Stack). Di dalam website ini juga terdapat API seperti MapBox untuk dapat melihat lokasi cabang cafe pada sebuah Map dan API google oauth agar user dapat login dengan akun google mereka.
               </p>
-              <a href='https://labuenacafe.herokuapp.com/labuenacafe' target='_blank' className='visit-button'>Visit Site</a>
+              <a href='https://labuenacafe.up.railway.app/labuenacafe' target='_blank' className='visit-button'>Visit Site</a>
               <a href='https://github.com/nesngenes/labuenacafeMERN' target='_blank' className='source-code'>Source Code</a>
             </div>
           </div>
+          
+
+          <div className='project'>
+            <div className='project-title'>
+              <p>Company site</p>
+              <h1>Albatech</h1>
+              <div className='image-wrapper'>
+              <img src={albatech} />
+              </div>
+            </div>
+            <div className='project-cotent'>
+              <p className='project-description'>
+              Menerjakan soal Technical Test dari perusahaan Albatech yaitu dengan mengubah desain Figma yang telah diberikkan perusahaan menjadi clean code dengan React JS. Serta menampilkan external API jika user login dengan email eve.holt@reqres.in dan password dapat diisi bebas.
+              </p>
+              <a href='https://frontendtest-production.up.railway.app/albatech' target='_blank' className='visit-button'>Visit Site</a>
+              <a href='https://github.com/nesngenes/FrontEndTest' target='_blank' className='source-code'>Source Code</a>
+            </div>
+          </div>
+
 
           <div className='project'>
             <div className='project-title'>
@@ -41,7 +60,7 @@ const Projects = () => {
               <p className='project-description'>
               Aone merupakan website e-commer atau online shop yang menjual perlengkapan olahraga voli. Aone dibuat menggunakan MERN Stack dan menggunakan API Stripe agar user dapat melakukan pembayaran.
               </p>
-              <a href='https://aoneapp.herokuapp.com/aone' target='_blank' className='visit-button'>Visit Site</a>
+              <a href='https://aone2-production.up.railway.app/aone' target='_blank' className='visit-button'>Visit Site</a>
               <a href='https://github.com/nesngenes/aone' target='_blank' className='source-code'>Source Code</a>
             </div>
           </div>
